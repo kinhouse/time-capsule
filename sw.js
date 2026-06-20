9ea3ca3a3803757eaf6032b3140edd181d3c98c7
rosenhouse <rosenhouse@gmail.com>
2026-06-20 12:36:50 +0000

    Deploying to gh-pages from @ kinhouse/Future@0950994987df69e97a5d52ffa0c9b5744359011a 🚀

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'future-cal-v1'
+
+self.addEventListener('install', () => self.skipWaiting())
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return
+  if (!e.request.url.startsWith(self.location.origin)) return
+
+  e.respondWith(
+    caches.open(CACHE).then(async cache => {
+      const cached = await cache.match(e.request)
+      const networkPromise = fetch(e.request).then(response => {
+        if (response.ok) cache.put(e.request, response.clone())
+        return response
+      }).catch(() => null)
+
+      return cached ?? await networkPromise
+    })
+  )
+})
